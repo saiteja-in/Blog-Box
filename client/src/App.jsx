447f332eaa0d78b06873d18e133e967da8f55023
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-20 17:52:05 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 const App = () => {
-  return <div className="text-3xl">sai teja</div>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path='/sign-in' element={<Signin/>}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
+      <Route path='/About' element={<About/>}></Route>
+      <Route path='/Dashboard' element={<Dashboard/>}></Route>
+      <Route path='/Projects' element={<Projects/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
