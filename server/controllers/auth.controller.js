4f77677a98df49bd073b9f84857c51f89ddf25fd
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-21 12:37:43 +0530

    create middlware for errors and custom function for custom errors

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,14 +1,20 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
-export const signup=async(req,res)=>{
+export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     if(!username || !email || !password || username===""||email===""||password===""){
-        res.status(400).json({message:"All fields are required"});
+        next(errorHandler(400,"All fields are required"));
+        return;
     }
-    const hashedPassword=bcryptjs.hashSync(password,10);
-    const user=await User.create({username,email,password:hashedPassword});
-    res.status(201).json({message:"User created successfully",user});
+    try {
+        const hashedPassword=bcryptjs.hashSync(password,10);
+        const user=await User.create({username,email,password:hashedPassword});
+        res.status(201).json({message:"User created successfully",user});
+    } catch (error) {
+        next(error)
+    }
 }
 export const signin=async(req,res)=>{
     const {email,password}=req.body;
