a7572e1708519ba7b69c6a6622b21fe6f42071b2
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-21 11:45:06 +0530

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-import express from 'express';
-const app=express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to mongoose");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
 
-const PORT=3000;
+const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-
-app.listen(PORT,()=>console.log("server started"))
+app.listen(PORT, () => console.log("server started"));
