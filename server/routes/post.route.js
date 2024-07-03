56b1a5faa97577ecc98212c1658ed81af51ed014
saiteja-in <vurukondasaiteja13@gmail.com>
2024-07-03 22:20:51 +0530

    get posts api route(very important)

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { verifyUser } from '../utils/verifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.post('/create-post',verifyUser, createPost);
-
+router.get('/getPosts',getPosts);
 export default router;
