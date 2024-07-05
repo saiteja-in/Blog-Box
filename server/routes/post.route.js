2c3ba938f0d8303f2d060dd5204904d056726502
saiteja-in <vurukondasaiteja13@gmail.com>
2024-07-05 22:13:38 +0530

    update post functionality

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyUser } from '../utils/verifyUser.js';
-import { createPost, deletePost, getPosts } from '../controllers/post.controller.js';
+import { createPost, deletePost, getPosts, updatePost } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.post('/create-post',verifyUser, createPost);
 router.get('/getPosts',getPosts);
 router.delete('/deletePost/:postId/:userId',verifyUser,deletePost);
+router.put('/updatePost/:postId/:userId',verifyUser,updatePost);
 export default router;
