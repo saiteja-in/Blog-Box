ca50e82a5027b04dffeb982c8444eca35ddd8261
saiteja-in <vurukondasaiteja13@gmail.com>
2024-09-06 21:33:19 +0530

    modified light theme

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-const ThemeProvider = ({children}) => {
-    const {theme}=useSelector((state)=>state.theme);
-  return (
-    <div className={theme}>
-        {/* <div className='relative min-h-screen bg-background font-sans antialiased'> */}
-        <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen'>
-      {children}
-    </div>
-    </div>
-  )
+
+const ThemeProvider = ({ children }) => {
+    const { theme } = useSelector((state) => state.theme);
+
+    return (
+        <div className={theme}>
+            {theme === 'light' && (
+                <div
+                    className="fixed inset-0 z-[-1] w-screen h-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
+                </div>
+            )}
+            <div className='relative min-h-screen text-gray-700 dark:text-gray-200 dark:bg-black'>
+                {children}
+            </div>
+        </div>
+    );
 }
 
-export default ThemeProvider
+export default ThemeProvider;
